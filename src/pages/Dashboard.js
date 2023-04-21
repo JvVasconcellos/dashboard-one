851a26e26369418cde235e1dc657b0190750468a
Joao Victor <joao.vasconcellos@engenharia.ufjf.br>
2023-04-21 20:04:03 +0000

    Atualização da page Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,54 @@
 import React from "react";
+import Header from "../components/layout/Header";
+import LineChart from "../components/charts/LineChart";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import "../styles/Dashboard.css"
 
-export default function Dashboard(props){
+
+export default function Dashboard() {
+  const props = {
+    "identifier": "Teste",
+    "datasets": [
+      {
+        id: 'dataset1',
+        data: [
+          { date: new Date(2022, 0, 1), value: 10 },
+          { date: new Date(2022, 1, 1), value: 20 },
+          { date: new Date(2022, 2, 1), value: 15 },
+          { date: new Date(2022, 3, 1), value: 25 },
+        ],
+        color: 'steelblue',
+      },
+      {
+        id: 'dataset2',
+        data: [
+          { date: new Date(2022, 0, 1), value: 15 },
+          { date: new Date(2022, 1, 1), value: 18 },
+          { date: new Date(2022, 2, 1), value: 18 },
+          { date: new Date(2022, 3, 1), value: 30 },
+        ],
+        color: 'green',
+      }
+    ],
+    "config": {
+      "keyLabel": "Data",
+      "valueLabel": "Valor (R$)",
+      "keyType": "date"
+    }
+  };
+
+  const margin = { top: 100, right: 100, bottom: 100, left: 100 };
+
+
+  return (
+    <div>
+      <Header title={props.identifier} />
+      <div style={{width: '33%', height: '400px'}}>
+        <ParentSize>
+          {({width, height}) =>  <LineChart datasets={props.datasets} width={width} height={height} margin={margin} config={props.config}/>}
+        </ParentSize>
+      </div>
+    </div>
     
-    return (
-        <div className="App">
-          <h1>Hello CodeSandbox</h1>
-          <h2>Start editing to see some magic happen!</h2>
-        </div>
-      );
-
+  );
 }
